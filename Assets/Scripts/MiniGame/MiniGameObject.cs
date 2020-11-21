30e2c58ff4f31d38057a7340b049b096e871633f
TheRaizer <aidan.fu000@gmail.com>
2020-11-20 23:36:59 -0500

    Changed miniGame object from button to click detection

--- Assets/Scripts/MiniGame/MiniGameObject.cs
+++ Assets/Scripts/MiniGame/MiniGameObject.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class MiniGameObject : MonoBehaviour
+public class MiniGameObject : MonoBehaviour, IPointerDownHandler
 {
     [SerializeField] private float fallSpeed = 0f;
     private MiniGameManager miniGameManager;
@@ -18,7 +19,7 @@ public class MiniGameObject : MonoBehaviour
         rect.Translate(new Vector3(0, -fallSpeed * Time.deltaTime), Space.Self);
     }
 
-    public void OnPress()
+    public void OnPointerDown(PointerEventData eventData)
     {
         miniGameManager.ChangeMiniGame(MiniGame);
         gameObject.SetActive(false);
